0ae0a5bf01cabe4107979bb2b0d22e62fb44066c
LeninRod012 <leninrodriguezmaria@gmail.com>
2025-02-08 18:47:04 -0400

    Actualiza el app.js

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+let hora = 0;
+hora = prompt("Qu'e hora es?")
+alert("Son las "+hora)
+
+
+
+//variables  y arrays
+//let amigos [];
+
+
+//FUNCIONES
+/*Esta funcion permitira que el usuario agregue un nombre en el campo 'Escribe un nombre'
+y que lo pueda agregar al array amigos al darle al boton anadir amigos*/
+function agregarAmigo(){
+var amigo = document.getElementById("amigo")
+};
+
+
+
+
+
+
+
+
+
+function sortearAmigo()
+{
+alert("Activaste la función sortear amigo")
+};
+function agregarAmigo(){
+    alert('activaste la función agregar amigo')
+};
+alert('el juego está vivo');
+
+
+
