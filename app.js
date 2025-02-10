5487f25ae8c31a4da8d8ec8746edde402ad78a7d
LeninRod012 <leninrodriguezmaria@gmail.com>
2025-02-09 21:30:46 -0400

    Se modifica el app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 
+let amigos =[];
 
 
 
@@ -14,9 +15,14 @@ y que lo pueda agregar al array amigos al darle al boton anadir amigos*/
 
 function agregarAmigo(){
 
-    dinero = document.querySelector('input');
-alert(dinero);
-//var amigo[].push = document.getElementById("amigo")
+    /*esta funcion, al darle click al boton añadir te permite agregar
+    un amigo que se almacena en el ultimo valor del array amigos. Luego
+    Imprime el ultimo amigo y te dice la longitud actual del array*/
+
+   amigos.push(prompt("Introduce un amigo")) ;
+console.log(amigos[amigos.length-1]);
+console.log(amigos.length);
+
 };
 
 
@@ -29,7 +35,20 @@ alert(dinero);
 
 function sortearAmigo()
 {
-alert("Activaste la función sortear amigo")
+/*esta funciona primera identifica si no se han agregado amigos, y te da
+una alerta en caso de. En caso que se hayan agregado amigos, los soltea y
+pone un alert un amigo aleatorio*/
+
+
+if (amigos.length == 0) {
+   alert( "No hay amigos que soltear")
+}
+else{
+amigoSecreto = Math.floor(Math.random()*amigos.length);
+console.log(amigoSecreto);
+alert(`El amigo secreto es ${amigos[amigoSecreto]}`);
+};
+
 };
 /*function agregarAmigo(){
     alert('activaste la función agregar amigo')
