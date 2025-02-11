af8a54df10d5e9138b1c2404fbcea2cfe48f6d11
LeninRod012 <leninrodriguezmaria@gmail.com>
2025-02-10 21:49:23 -0400

    Modifica el app.js. Le agrega las conexiones a los elementos HTML

--- app.js
+++ app.js
@@ -21,10 +21,21 @@ function agregarAmigo(){
     un amigo que se almacena en el ultimo valor del array amigos. Luego
     Imprime el ultimo amigo y te dice la longitud actual del array*/
 
-   amigos.push(prompt("Introduce un amigo")) ;
+
+var amigoNuevo = document.getElementById("amigo"); //me permite asignarle el elemento con ese ID a una variable de JS.
+
+
+if (amigoNuevo.value==""){ //confirma que el nombre agregado no esté vacío
+    alert("Ingrese un nombre valido")
+}
+else{
+
+   amigos.push(amigoNuevo.value); // Me permite agregar el *value* en ese input a el array amigos
+   amigoNuevo.value= ""; // borra lo que hay en el input para poder escribir de nuevo.
 console.log(amigos[amigos.length-1]);
 console.log(amigos.length);
-agregarALista();
+agregarALista(); // ejecuta la funcion Agregar a la lista
+}
 };
 
 
@@ -73,7 +84,11 @@ if (amigos.length == 0) {
 else{
 amigoSecreto = Math.floor(Math.random()*amigos.length);
 console.log(amigoSecreto);
-alert(`El amigo secreto es ${amigos[amigoSecreto]}`);
+const resultadoFinal = document.getElementById("resultado");
+/*const borrarLista = document.getElementById("amigos");
+borrarLista.textContent= "";*/
+resultadoFinal.textContent= `El amigo secreato es ${amigos[amigoSecreto]}`;
+//alert(`El amigo secreto es ${amigos[amigoSecreto]}`);
 };
 
 };
